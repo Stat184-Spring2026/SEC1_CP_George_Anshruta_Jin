afcb6705bba5b7a1d8a7c4686d9a02efeb59777f
asr5768 <asr5768@psu.edu>
2026-05-06 16:28:13 -0400

    updated code for graph and table

--- project.R
+++ project.R
@@ -3,27 +3,40 @@ library(tidyverse)
 library(rvest)
 library(ggplot2)
 
-#loading the retatrutide dataset from the gov website
-RetatrutideRawData <- read_html(
-  x = "https://pmc.ncbi.nlm.nih.gov/articles/PMC12026077/"
-)|>
 
-  html_elements(css="table") |>
-  html_table()
-
-#loading the specific table
-RetatrutideEffects <- RetatrutideRawData[[4]]
+#loading the dataset
+RetatrutideEffects <- read_csv("cleaned_dataset_retatrutide.csv")
 
 
 ##cleaning data for retatrutide
 RetatrutideCData <- RetatrutideEffects |>
   mutate(
     RR = as.numeric(str_extract(`Pooled effect size, RR (95% CI)`, "^[0-9.]+")),
-    LowerCI = as.numeric(str_extract(`Pooled effect size, RR (95% CI)`, "(?<=\\()[0-9.]+")),
-    UpperCI = as.numeric(str_extract(`Pooled effect size, RR (95% CI)`, "(?<=, )[0-9.]+"))
+    LowerCI = as.numeric(str_match(`Pooled effect size, RR (95% CI)`, "\\(([0-9.]+),\\s*([0-9.]+)\\)")[,2]),
+    UpperCI = as.numeric(str_match(`Pooled effect size, RR (95% CI)`, "\\(([0-9.]+),\\s*([0-9.]+)\\)")[,3])
   )
 
 
+##table
+RetatrutideSummary <- RetatrutideCData |>
+  mutate(
+    Significant = ifelse(LowerCI > 1, "Yes", "No")
+  )|>
+  group_by(`Outcome variables`) |>
+  slice_max(RR, n = 1, with_ties = FALSE) |>
+  ungroup() |>
+  select(
+    `Outcome variables`,
+    `Retatrutide arm (mg/week)`,
+    RR,
+    LowerCI,
+    UpperCI,
+    Significant
+  )
+
+View(RetatrutideSummary)
+
+##graph on the risk ratio of effects of taking retatrutide
 ggplot(RetatrutideCData,
        aes(
          x = RR,
